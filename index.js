d34b467bdd6706379aaddbb5749cb50b522dc06a
Sean Rooney <seandanielrooney@gmail.com>
2020-02-28 08:43:44 -0700

    setting things up

---
+++ index.js
@@ -0,0 +1,83 @@
+// const questions = [
+
+// ];
+
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
+
+//Links inquirer and fs packages
+var inquirer = require("inquirer");
+var fs = require('fs');
+
+//user inquirer package to set up a prompt
+inquirer.prompt([
+{
+    
+    type: "input",
+    name: "name",
+    message: "What is your GitHub username?"
+},
+{
+     
+     type: "input",
+     name: "title",
+     message: "What is the name of the project?"
+
+},
+{
+     type: "input",
+     name: "description",
+     message: "Can you give me a summary of the project?"
+},
+{
+    type: "input",
+    name: "installation",
+    message: "How do I install this program?"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "How do I use this program?"
+},
+{
+    type: "input",
+    name: "license",
+    message: "What is the licensing information?"
+},
+{
+    type: "input",
+    name: "contributors",
+    message: "How can one contribute to this?"
+},
+{
+    type: "input",
+    name: "tests",
+    message: "How do you test this program?"
+},
+{
+    type: "input",
+    name: "questions",
+    message: "Honestly not sure what you mean by 'questions' for this one"
+},
+
+]).then(function(data) {
+    const readMe = `
+    
+    `
+    var fileName = data.name.toLowerCase().split(' ').join('') + ".md";
+    
+    fs.writeFile(fileName, readMe, function(err) {
+       
+        if (err) {
+            return console.log(err);
+        }
+    
+        console.log("You did it!");
+    });
+});
