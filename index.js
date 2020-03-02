df8df011e849373c4d28cb9cef5efbaeed7bf9a4
Sean Rooney <seandanielrooney@gmail.com>
2020-03-01 20:07:48 -0700

    fixing questions and working on structure

--- index.js
+++ index.js
@@ -1,94 +1,88 @@
-const questions = [
-
-];
-
-// function writeToFile(fileName, data) {
-// }
-
-// function init() {
-
-// }
-
-// init();
-
-//Links inquirer and fs packages
-
-//MAKE SURE TO USE AXIOS!!!!!!
 const inquirer = require("inquirer");
 const fs = require('fs');
 const axios = require("axios");
 const generateMarkdown = require("./utils/generateMarkdown.js");
+// const queryUrl = `https://api.github.com/users/${username}`;
 
-//user inquirer package to set up a prompt
-inquirer.prompt([
-{
-    
-    type: "input",
-    name: "name",
-    message: "What is your GitHub username?"
-},
-{
-     
-     type: "input",
-     name: "title",
-     message: "What is the name of the project?"
+console.log("This is a readme generator that generates a dynamic readme and pulls profile data from your GitHub account.  You will be prompted to fill out information to place in the readme once it is generated.");
 
-},
-{
-     type: "input",
-     name: "description",
-     message: "Can you give me a summary of the project?"
-},
-{
-    type: "input",
-    name: "installation",
-    message: "How do I install this program?"
-},
-{
-    type: "input",
-    name: "usage",
-    message: "How do I use this program?"
-},
-{
-    type: "input",
-    name: "license",
-    message: "What is the licensing information?"
-},
-{
-    type: "input",
-    name: "contributors",
-    message: "How can one contribute to this?"
-},
-{
-    type: "input",
-    name: "tests",
-    message: "How do you test this program?"
-},
-{
-    type: "input",
-    name: "questions",
-    message: "Honestly not sure what you mean by 'questions' for this one"
-},
+const questions = [
+    
+];
+        inquirer.prompt([
+        {
+            
+            type: "input",
+            name: "name",
+            message: "What is your GitHub username?"
+        },
+        {
+             
+             type: "input",
+             name: "title",
+             message: "What is the name of the project?"
+        
+        },
+        {
+             type: "input",
+             name: "description",
+             message: "Can you give me a summary of the project?"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "How do I install this program?"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "How do I use this program?"
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "What is the licensing information?"
+        },
+        {
+            type: "input",
+            name: "contributors",
+            message: "Who contributed to this?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "How do you test this program?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "Is there anything else you would like to add to this readme?"
+        },
+        
+        ]).then(function(data) {
+            const readMe = `
+            ${data.title}
+            ${data.name}
+            ${data.description}
+            ${data.installation}
+            ${data.license}
+            ${data.tests}
+            ${data.questions}
+            
+            `
+            var fileName = data.name.toLowerCase().split(' ').join('') + ".md";
+            
+            fs.writeFile(fileName, readMe, function(err) {
+               
+                if (err) {
+                    return console.log(err);
+                }
+            
+                console.log("Readme Generated!");
+            });
+        });
 
-]).then(function(data) {
-    const readMe = `
-    ${data.name}
-    ${data.title}
-    ${data.description}
-    ${data.installation}
-    ${data.license}
-    ${data.tests}
-    ${data.questions}
-    
-    `
-    var fileName = data.name.toLowerCase().split(' ').join('') + ".md";
-    
-    fs.writeFile(fileName, readMe, function(err) {
-       
-        if (err) {
-            return console.log(err);
-        }
-    
-        console.log("You did it!");
-    });
-});
+
+
+
+
